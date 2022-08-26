c025fce4cf7f701b857834f6a3af816b92514444
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-08-25 21:19:38 -0300

    ✅feat: add player IA

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,15 @@ for (let i = 0; i < boxes.length; i++) {
       let cloneElemento = elemento.cloneNode(true);
       this.appendChild(cloneElemento);
 
-      //computar a jpgada
+      //computar a jogada
       if (player1 == player2) {
         player1++;
+
+        if (secondPlayer == "ai-player") {
+          // função execura a rodada
+          computerPlay();
+          player2++;
+        }
       } else {
         player2++;
       }
@@ -31,6 +37,20 @@ for (let i = 0; i < boxes.length; i++) {
       // checa quem venceu
       checkWinCondition();
     }
+  });
+}
+
+//evento para saber se é 2 player ou IA
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    secondPlayer = this.getAttribute("id");
+    for (let j = 0; j < buttons.length; j++) {
+      buttons[j].style.display = "none";
+    }
+    setTimeout(function () {
+      let container = document.querySelector("#container");
+      container.classList.remove("hide");
+    }, 500);
   });
 }
 // ver quem vai jogar
@@ -234,5 +254,32 @@ function declareWinner(winner) {
 
   for (let i = 0; i < boxesToRemove.length; i++) {
     boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+  }
+}
+
+//executar a logica da jogada do IA
+
+function computerPlay() {
+  let cloneO = o.cloneNode(true);
+  counter = 0;
+  filled = 0;
+
+  for (let i = 0; i < boxes.length; i++) {
+    let randonNumber = Math.floor(Math.random() * 5);
+
+    // só preencher se estiver vazio o filho
+    if (boxes[i].childNodes[0] == undefined) {
+      if (randonNumber <= 1) {
+        boxes[i].appendChild(cloneO);
+        counter++;
+        break;
+      }
+      //checagem de quantas estão preenchidas
+    } else {
+      filled++;
+    }
+  }
+  if(counter == 0 && filled < 9) {
+    computerPlay()
   }
 }
